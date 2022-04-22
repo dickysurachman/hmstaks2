39c3112321fd9ce53b20abadb9587ed6a4dc2b9b
dicky.surachman1@gmail.com <dicky.surachman1@gmail.com>
2022-04-22 09:58:40 +0700

    task2 ITTS

---
+++ app/src/main/java/itts/dicky/surachman/maps/MainActivity.java
@@ -0,0 +1,161 @@
+package itts.dicky.surachman.maps;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+import com.huawei.hmf.tasks.OnFailureListener;
+import com.huawei.hmf.tasks.OnSuccessListener;
+import com.huawei.hmf.tasks.Task;
+import com.huawei.hms.common.ApiException;
+import com.huawei.hms.support.account.AccountAuthManager;
+import com.huawei.hms.support.account.request.AccountAuthParams;
+import com.huawei.hms.support.account.request.AccountAuthParamsHelper;
+import com.huawei.hms.support.account.result.AuthAccount;
+import com.huawei.hms.support.account.service.AccountAuthService;
+
+public class MainActivity extends AppCompatActivity {
+    private AccountAuthService mAuthService,mAuthManager;
+
+    // Specify the user information to be obtained after user authorization.
+    private AccountAuthParams mAuthParam;
+    private static boolean sign1=false;
+    // Define the request code for signInIntent.
+    private static final int REQUEST_CODE_SIGN_IN = 1000;
+
+    // Define the log flag.
+    private static final String TAG = "Account";
+
+    /**
+     * Silent sign-in: If a user has authorized your app and signed in, no authorization or sign-in screen will appear during subsequent sign-ins, and the user will directly sign in.
+     * After a successful silent sign-in, the HUAWEI ID information will be returned in the success event listener.
+     * If the user has not authorized your app or signed in, the silent sign-in will fail. In this case, your app will show the authorization or sign-in screen to the user.
+     */
+    private void silentSignIn() {
+        mAuthParam = new AccountAuthParamsHelper(AccountAuthParams.DEFAULT_AUTH_REQUEST_PARAM)
+                .setProfile()
+                .setEmail()
+                .createParams();
+
+        // Use AccountAuthParams to build AccountAuthService.
+        mAuthManager = AccountAuthManager.getService(this, mAuthParam);
+
+        // Sign in with a HUAWEI ID silently.
+        //Task<AuthAccount> task = mAuthService.silentSignIn();// .silentSignIn();
+        Task<AuthAccount> task = mAuthManager.silentSignIn();
+        task.addOnSuccessListener(new OnSuccessListener<AuthAccount>() {
+            @Override
+            public void onSuccess(AuthAccount authAccount) {
+                Log.i(TAG, "silentSignIn success");
+            }
+        });
+        task.addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(Exception e) {
+                //if Failed use getSignInIntent
+                if (e instanceof ApiException) {
+                    ApiException apiException = (ApiException) e;
+                    signIn();
+                }
+            }
+        });
+    }
+    private void signIn() {
+        mAuthParam = new AccountAuthParamsHelper(AccountAuthParams.DEFAULT_AUTH_REQUEST_PARAM)
+                .setIdToken()
+                .setAccessToken()
+                .createParams();
+        mAuthManager = AccountAuthManager.getService(MainActivity.this, mAuthParam);
+        //startActivityForResult(mAuthManager.getSignInIntent(), Constant.REQUEST_SIGN_IN_LOGIN);
+    }
+
+    private void silentSignInByHwId() {
+        //  1. Use AccountAuthParams to specify the user information to be obtained after user authorization, including the user ID (OpenID and UnionID), email address, and profile (nickname and picture).
+        // 2. By default, DEFAULT_AUTH_REQUEST_PARAM specifies two items to be obtained, that is, the user ID and profile.
+        // 3. If your app needs to obtain the user's email address, call setEmail().
+        // 4. To support authorization code-based HUAWEI ID sign-in, use setAuthorizationCode(). All user information that your app is authorized to access can be obtained through the relevant API provided by the Account Kit server.
+        mAuthParam = new AccountAuthParamsHelper(AccountAuthParams.DEFAULT_AUTH_REQUEST_PARAM)
+                .setProfile()
+                .setEmail()
+                .createParams();
+
+        // Use AccountAuthParams to build AccountAuthService.
+        mAuthService = AccountAuthManager.getService(this, mAuthParam);
+
+        // Sign in with a HUAWEI ID silently.
+        Task<AuthAccount> task = mAuthService.silentSignIn();// .silentSignIn();
+        task.addOnSuccessListener(new OnSuccessListener<AuthAccount>() {
+            @Override
+            public void onSuccess(AuthAccount authAccount) {
+                sign1 =true;
+                // The silent sign-in is successful. Process the returned AuthAccount object to obtain the HUAWEI ID information.
+                findViewById(R.id.loadingPanel).setVisibility(View.GONE);
+                findViewById(R.id.HuaweiIdAuthButton).setVisibility(View.VISIBLE);
+                Intent goto3 = new Intent(MainActivity.this,loginsuccess.class );
+                goto3.putExtra(loginsuccess.Name,authAccount.getDisplayName());
+                goto3.putExtra(loginsuccess.Email1,authAccount.getEmail());
+                goto3.putExtra(loginsuccess.Gambar,authAccount.getAvatarUriString());
+                startActivity(goto3);
+                dealWithResultOfSignIn(authAccount);
+            }
+        });
+        task.addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(Exception e) {
+                // The silent sign-in fails. Your app will call getSignInIntent() to show the authorization or sign-in screen.
+                if (e instanceof ApiException) {
+                    ApiException apiException = (ApiException) e;
+                    Intent signInIntent = mAuthService.getSignInIntent();
+                    // If your app appears in full screen mode when a user tries to sign in, that is, with no status bar at the top of the device screen, add the following parameter in the intent:
+                    // intent.putExtra(CommonConstant.RequestParams.IS_FULL_SCREEN, true);
+                    // Check the details in this FAQ.
+                    //signInIntent.putExtra(CommonConstant.RequestParams.IS_FULL_SCREEN, true);
+                    startActivityForResult(signInIntent, REQUEST_CODE_SIGN_IN);
+                    Toast.makeText(getApplicationContext(), "Error Login " ,Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    /**
+     * Process the returned AuthAccount object to obtain the HUAWEI ID information.
+     *
+     * @param authAccount AuthAccount object, which contains the HUAWEI ID information.
+     */
+    private void dealWithResultOfSignIn(AuthAccount authAccount) {
+        Toast.makeText(getApplicationContext(), "Welcome "+authAccount.getDisplayName() ,Toast.LENGTH_SHORT).show();
+        Log.i(TAG,"code:"+authAccount.getAuthorizationCode());
+        Log.i(TAG, "display name:" + authAccount.getDisplayName());
+        Log.i(TAG, "photo uri string:" + authAccount.getAvatarUriString());
+        Log.i(TAG, "photo uri:" + authAccount.getAvatarUri());
+        Log.i(TAG, "email:" + authAccount.getEmail());
+        Log.i(TAG, "openid:" + authAccount.getOpenId());
+        Log.i(TAG, "unionid:" + authAccount.getUnionId());
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        findViewById(R.id.HuaweiIdAuthButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                findViewById(R.id.loadingPanel).setVisibility(View.VISIBLE);
+                findViewById(R.id.HuaweiIdAuthButton).setVisibility(View.GONE);
+                Toast.makeText(getApplicationContext(), "Please wait, it may take a few minute...",Toast.LENGTH_SHORT).show();
+                silentSignInByHwId();
+            }
+        });
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if(sign1==false) {
+            silentSignInByHwId();
+        }
+    }
+}
---
+++ app/src/main/java/itts/dicky/surachman/maps/loginsuccess.java
@@ -0,0 +1,117 @@
+package itts.dicky.surachman.maps;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.PermissionChecker;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
+import android.os.Build;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+public class loginsuccess extends AppCompatActivity {
+    public static final String Name = "Name";
+    public static final String Gambar = "Picture";
+    public static final String Email1 = "Email";
+    private Context context;
+    private static final int GET_BY_CROP = 804;
+    private static final int GET_BY_ALBUM1 = 801;
+    private static final int GET_BY_CAMERA = 805;
+    List<String> mPermissionList = new ArrayList<>();
+    String[] permissions = new String[] {Manifest.permission.READ_PHONE_STATE,
+            Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE};
+    private final int mRequestCode = 100;
+
+    TextView teks;
+    Button but1,but2;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_loginsuccess);
+        teks = findViewById(R.id.textView);
+        String name1 = getIntent().getStringExtra(Name);
+        String gambar1 = getIntent().getStringExtra(Gambar);
+        String emails = getIntent().getStringExtra(Email1);
+        teks.setText("Welcome "+  name1 +"\n"+"Your Email "+  emails );
+        but1 = findViewById(R.id.maps);
+        but1.setOnClickListener(this);
+        but2 = findViewById(R.id.imagekit2);
+        but2.setOnClickListener(this);
+        //Gbr = findViewById(R.id.imageView);
+        new DownloadImageFromInternet((ImageView) findViewById(R.id.imageView)).execute(gambar1);
+
+        if (Build.VERSION.SDK_INT >= 23) {
+            initPermission();
+        }
+
+    }
+    private void initPermission() {
+        // Clear the permissions that fail the verification.
+        mPermissionList.clear();
+        //Check whether the required permissions are granted.
+        for (int i = 0; i < permissions.length; i++) {
+            if (PermissionChecker.checkSelfPermission(this, permissions[i])
+                    != PermissionChecker.PERMISSION_GRANTED) {
+                // Add permissions that have not been granted.
+                mPermissionList.add(permissions[i]);
+            }
+        }
+        //Apply for permissions.
+        if (mPermissionList.size() > 0) {//The permission has not been granted. Please apply for the permission.
+            ActivityCompat.requestPermissions(this, permissions, mRequestCode);
+        }
+    }
+
+    private class DownloadImageFromInternet extends AsyncTask<String, Void, Bitmap> {
+        ImageView imageView;
+        public DownloadImageFromInternet(ImageView imageView) {
+            this.imageView=imageView;
+            Toast.makeText(getApplicationContext(), "Please wait, it may take a few minute...",Toast.LENGTH_SHORT).show();
+        }
+        protected Bitmap doInBackground(String... urls) {
+            String imageURL=urls[0];
+            Bitmap bimage=null;
+            try {
+                InputStream in=new java.net.URL(imageURL).openStream();
+                bimage= BitmapFactory.decodeStream(in);
+            } catch (Exception e) {
+                Log.e("Error Message", e.getMessage());
+                e.printStackTrace();
+            }
+            return bimage;
+        }
+        protected void onPostExecute(Bitmap result) {
+            imageView.setImageBitmap(result);
+        }
+    }
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.maps:
+                getByAlbum(LoginSuccess.this, GET_BY_CROP);
+                break;
+            case R.id.imagekit2:
+                Toast.makeText(LoginSuccess.this, "Please wait for download sample", Toast.LENGTH_LONG)
+                        .show();
+                Intent intent4 = new Intent(LoginSuccess.this, imagekit.class);
+                intent4.putExtra(imagekit.gambarcon,"https://www.intiwhiz.com/themenew/img/slide/1.jpg");
+                startActivity(intent4);
+                break;
+        }
+    }
+}
